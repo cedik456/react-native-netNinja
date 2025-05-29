1ab39a98b3c8e1e67f725e9119e075010eb75f4c
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-05-29 11:51:59 +0800

    Update profile component

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -4,11 +4,11 @@ import Button from "../../components/Button";
 import { useAuth } from "../../hooks/useAuth";
 
 const Profile = () => {
-  const { logout } = useAuth();
+  const { logout, user } = useAuth();
 
   return (
     <View className="items-center justify-center flex-1 bg-white">
-      <Text className="mb-20">Your email</Text>
+      <Text className="mb-20">Welcome {user.email}</Text>
       <Text className="mb-20">Time to start reading books</Text>
       <Button onPress={logout}>Logout</Button>
     </View>
